b7816f8ec12ac47b9c3ae66ec0d8d1b7e58b6830
NikoArc42 <nikoarc42@gmail.com>
2025-10-15 22:25:41 +0200

    gwooba

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+const popContainer = document.querySelector("#popContainer");
+const showPopButton = document.querySelector("#showPop");
+const taskButton = document.querySelector("#taskButton");
+const taskListen = document.querySelector("#taskList");
+const taskText = document.querySelector("#taskText");
+
+showPopButton.addEventListener("click", () => {
+    popContainer.classList.toggle("hidden");
+});
+
+taskButton.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    
+    
+})
